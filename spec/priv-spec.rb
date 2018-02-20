c7c086d2f7bc8ad576940f2c5cbd497bd5798a22
Ben Batschelet <bartleby84@gmail.com>
2018-02-20 16:04:49 -0600

    Add privileges

---
+++ spec/priv-spec.rb
@@ -0,0 +1,37 @@
+require_relative "bootstrap"
+
+RSpec.configure do |config|
+  config.before :suite do
+    AnsibleHelper.playbook("playbooks/install-postgresql.yml", ENV["TARGET_HOST"], {
+      install_postgres: true,
+      new_db_name:      "priv_test_db"
+    })
+    AnsibleHelper.playbook("playbooks/install-postgresql.yml", ENV["TARGET_HOST"], {
+      install_postgres:  false,
+      new_db_user:       "test_ro_user",
+      new_db_pass:       "password",
+      new_db_name:       "priv_test_db",
+      new_table_priv:    "SELECT",
+      new_sequence_priv: "SELECT"
+    })
+  end
+end
+
+context "PostgreSQL" do
+  include_examples "postgres server"
+  include_examples "postgres client", "9.6"
+end
+
+describe "New user" do
+  before(:context) do
+    set :env, :PGPASSWORD => 'password'
+    set :docker_container_exec_options, :Env => ["PGPASSWORD=password"]
+  end
+
+  let(:subject) { command %Q{psql -wtA -U test_ro_user priv_test_db -c "CREATE TABLE test_table (id integer PRIMARY KEY)"} }
+
+  it "failed to create a table" do
+    expect(subject.stderr).to match /permission denied for schema public/
+    expect(subject.exit_status).not_to eq 0
+  end
+end
